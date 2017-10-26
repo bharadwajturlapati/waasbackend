466ea525b8ee305144cfc415248f837c48f1fca8
bharadwajturlapati <tbj.name@gmail.com>
2017-10-26 22:17:19 +0530

    commiting final qucikaccess

--- WAAS/restApp/models.py
+++ WAAS/restApp/models.py
@@ -5,4 +5,4 @@ class Location(models.Model):
     id = models.AutoField(primary_key=True)
     fileurl = models.CharField(max_length=2048)
     nickname = models.CharField(max_length=100)
-    logicalgroup = models.CharField(max_length=100)
+    logicalgroup = models.CharField(max_length=100, blank=True)
---
+++ startmanagepy.cmd
@@ -0,0 +1,6 @@
+cd ../../
+cd waasenv_3_5_0
+call activate.cmd
+cd C:\Users\kh2034\PycharmProjects\workspace-backend
+cd WAAS
+manage.py runserver
